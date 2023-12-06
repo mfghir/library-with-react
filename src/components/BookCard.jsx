b447ee31aa3edff0448a508f14f5bcce7e5158f5
mfghir <fatemeghafari77@gmail.com>
2023-12-05 23:39:32 -0800

    add bookcard + style to it

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,9 +1,19 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { AiFillHeart } from "react-icons/ai";
+import styles from "./BookCard.module.css";
 
 const BookCard = ({ data: { title, author, image, language, pages } }) => {
+  const [like, setLike] = useState(false);
+
+  const likeHandler = () => {
+    setLike((like) => !like);
+  };
+
   return (
-    <div>
+    <div className={styles.card}>
       <img src={image} alt={title} />
-      <div>
+      <div className={styles.info}>
         <h3>{title}</h3>
         <p>{author}</p>
 
@@ -12,7 +22,9 @@ const BookCard = ({ data: { title, author, image, language, pages } }) => {
           <span>{pages} pages</span>
         </div>
 
-        <button>like</button>
+        <button onClick={likeHandler}>
+          <AiFillHeart color={like ? "red" : "#e0e0e0"} fontSize="1.8rem" />
+        </button>
       </div>
     </div>
   );
